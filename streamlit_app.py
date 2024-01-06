95e68d411b3ea726dbf5a3abed45132c26e1c502
Muhammad Wafa <muhammadwafa105@gmail.com>
2024-01-06 17:11:29 +0700

    Feat: implement semantic kernel and open ai with streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,15 @@
 import streamlit as st
-import random 
-import time
-from openai import OpenAI
-# from langchain.llms import OpenAI
+from miriko import Miriko
 
-#
-# Initialize
-#
-# Set OpenAI API key from Streamlit secrets
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+# setup Miriko
+openai_model="gpt-3.5-turbo"
+api_key=st.secrets["OPENAI_API_KEY"]
+org_id=st.secrets["OPENAI_ORG_ID"]
 
-# Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+if "miriko" not in st.session_state:
+    st.session_state.miriko = Miriko(openai_model, api_key, org_id)
 
+miriko = st.session_state.miriko if "miriko" in st.session_state else Miriko(openai_model, api_key, org_id)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -22,43 +18,65 @@ if "messages" not in st.session_state:
 #
 # Web
 #
+st.set_page_config(layout="wide", page_title="Miriko Bot", page_icon=":robot_face:")
 st.title("Miriko Bot")
 
 # display initial message
-with st.chat_message("assistant"):
-    st.write("Hello i am Miriko, your personal assistant. How can i help you?")
+st.write("Hello i am Miriko, your personal assistant. How can i help you?")
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+col1, col2 = st.columns(2)
 
-# Reeact to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # add user message to history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+with col1:   
+    with st.container(border=True):
+        # React to user input
+        prompt = st.text_area(label = "What is up?", key="user_input")
+        
+        if prompt is not None and prompt != "":
+            # add user message to history
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        assistant_response = random.choice(
-                [
-                "Hello there! How can I assist you today?",
-                "Hi, human! Is there anything I can help you with?",
-                "Do you need help?",
-            ]
-        )
-        # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                # create initial result placeholder
+                message_placeholder = st.empty()
+                full_response = ""
+                
+                for response in miriko.chat(prompt):
+                    full_response += response.choices[0].delta.content or ""
+                    message_placeholder.markdown(full_response + "")
+                    message_placeholder.markdown(full_response)
+                message_placeholder.markdown(full_response)
+                
+                st.session_state.messages.append({"role": "assistant", "content": full_response})
+                    
+    
+    with st.expander("Show Miriko's Memory"):
+        messages = miriko.get_all_miriko_memory()
+        st.write(messages)
+            
+    st.subheader("Chat History")
+    with st.container():
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])            
+                
+with col2:
+    with st.container(border=True):
+        # select skills
+        skills = st.selectbox("Select Skill", ["Brainstorming"])
+        
+        # React to user input
+        if prompt := st.text_input("Enter your Ideas", key="idea_input"):
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            # add user message to history
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-        # add assistant response to history
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                with st.spinner("Thinking..."):
+                    full_response = miriko.brainstormer(prompt)
+                    message_placeholder.markdown(full_response)
